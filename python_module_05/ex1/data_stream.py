a06efbfd37a5f86a7cb9d157ea98389a1c0b5ac0
Mobinati <soha.mobina@gmail.com>
2026-08-21 21:15:08 +0200

    submit05

---
+++ python_module_05/ex1/data_stream.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class DataProcessor(ABC):
+    def __init__(self) -> None:
+        self.data: list[tuple[int, str]] = []
+        self.rank: int = 0
+
+
+    @abstractmethod
+    def validate(self, data: Any) -> bool:
+        pass
+
+
+    @abstractmethod
+    def ingest(self, data: Any) -> None:
+        try:
+            validate(data)
+        
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def output(self) -> tuple[int, str]:
+        if self.data:
+            return self.data.pop(0)
+        else:
+            raise IndexError("No data available")
+
+class DataStream():
+    def register_processor(self, proc: DataProcessor) -> None:
+
+
+    def process_stream(self, stream: list[typing.Any]) -> None:
+
+    def print_processors_stats(self) -> None:
+        
+
+
+
+
+class NumericProcessor(DataProcessor):
+    def validate(self, data = Any) -> bool:
+        if isinstance(data, (int, float)) and not isinstance(data, bool):
+            return True
+
+        if isinstance(data, list):
+            return all(
+                isinstance(item, (int, float)) and not isinstance(item, bool)
+                for item in data
+            )
+        
+        else:
+            return False
+
+        
+    def ingest(self, data: int | float | list[int | float] ) -> None:
+        if not self.validate(data):
+            raise TypeError("Improper numeric data")
+        
+        if isinstance(data, list):
+            for item in data:
+                self.data.append((self.rank, str(item)))
+                self.rank += 1
+        else:
+            self.data.append((self.rank, str(data)))
+            self.rank += 1
+
+
+    
+
+class TextProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, str):
+            return True
+        
+        if isinstance(data, list):
+            return (all(isinstance(item, str) for item in data))
+        
+        return False
+
+    def ingest(self, data: str | list[str]) -> None:
+        if not self.validate(data):
+            raise TypeError("Improper str data")
+
+        if isinstance(data, list):
+            for item in data:
+                self.data.append((self.rank, str(item)))
+                self.rank += 1
+        else:
+            self.data.append((self.rank, str(data)))
+            self.rank += 1
+            
+
+
+class LogProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, dict):
+            return all(
+                isinstance(key, str) and isinstance(value, str) 
+                for key, value in data.items())
+        
+        if isinstance(data, list):
+            return all(
+                isinstance(item , dict)
+                and all(
+                    isinstance(key, str) and isinstance(value, str)
+                    for key, value in item.items()
+                ) 
+                for item in data
+            )
+        return False
+
+    def ingest(
+        self,
+        data: dict[str, str] | list[dict[str, str]]
+    ) -> None:
+        if not self.validate(data):
+            raise TypeError("Improper log data")
+        
+        if isinstance(data, list):
+            for item in data:
+                self.data.append((self.rank, str(item)))
+                self.rank += 1
+        if isinstance(data, dict):
+            self.data.append((self.rank, str(dict)))
+            self.rank += 1
+        
+
+
+def main() -> None:
+    print("=== Code Nexus - Data Processor ===")
+
+    # Numeric Processor
+    print("Testing Numeric Processor...")
+    numeric = NumericProcessor()
+
+    print(f"Trying to validate input '42': {numeric.validate(42)}")
+    print(f"Trying to validate input 'Hello': {numeric.validate('Hello')}")
+
+    print("Test invalid ingestion of string 'foo' without prior validation:")
+    try:
+        numeric.ingest("foo")  # type: ignore[arg-type]
+    except TypeError as error:
+        print(f"Got exception: {error}")
+
+    print("Processing data: [1, 2, 3, 4, 5]")
+    numeric.ingest([1, 2, 3, 4, 5])
+
+    print("Extracting 3 values...")
+    for _ in range(3):
+        rank, value = numeric.output()
+        print(f"Numeric value {rank}: {value}")
+
+    # Text Processor
+    print("Testing Text Processor...")
+    text = TextProcessor()
+
+    print(f"Trying to validate input '42': {text.validate(42)}")
+
+    print("Processing data: ['Hello', 'Nexus', 'World']")
+    text.ingest(["Hello", "Nexus", "World"])
+
+    print("Extracting 1 value...")
+    rank, value = text.output()
+    print(f"Text value {rank}: {value}")
+
+    # Log Processor
+    print("Testing Log Processor...")
+    log = LogProcessor()
+
+    print(f"Trying to validate input 'Hello': {log.validate('Hello')}")
+
+    logs = [
+        {
+            "log_level": "NOTICE",
+            "log_message": "Connection to server",
+        },
+        {
+            "log_level": "ERROR",
+            "log_message": "Unauthorized access!!",
+        },
+    ]
+
+    print(f"Processing data: {logs}")
+    log.ingest(logs)
+
+    print("Extracting 2 values...")
+    for _ in range(2):
+        rank, value = log.output()
+        print(f"Log entry {rank}: {value}")
+
+
+if __name__ == "__main__":
+    main()
